e8c41d62bffef7c8a8b6f57fc4275f79a61e1566
Yanin Vyacheslav <138291033+sygkanrt30@users.noreply.github.com>
2025-01-06 14:47:45 +0300

    Delete src/main/java/ru/pracise/oop/game directory

--- src/main/java/ru/pracise/oop/game/start_menu/CheckInputs.java
+++
@@ -1,7 +0,0 @@
-package ru.pracise.oop.game.start_menu;
-
-public class CheckInputs {
-    public static boolean checkChosen(String string) {
-        return string.equals("Log in") || string.equals("Log up");
-    }
-}
